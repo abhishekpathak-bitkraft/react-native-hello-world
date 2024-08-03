58fb2b2344bce50510ee1891954a462a4f298060
abhishekpathak-bitkraft <abhishek@bitkraft.co.in>
2024-08-03 18:05:22 +0530

    add shadow and elevation

--- App.tsx
+++ App.tsx
@@ -1,118 +1,55 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export class App extends Component {
+  render() {
+    return (
+      <View>
+        <Component1 />
+      </View>
+    );
+  }
+}
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const Component1 = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
+    <View style={[styles.viewStyle, styles.commonStyle, styles.boxShadow, styles.androidShadow]}>
+      <Text style={[styles.textStyle, styles.commonStyle]}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, maxime!
       </Text>
     </View>
   );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  viewStyle: {
+    width: 250,
+    height: 250,
+    borderWidth: 2,
+    borderColor: '#ff0000',
+    backgroundColor: '#ffaa00',
+    padding:10,
+    margin:20,
+    borderRadius:6
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  textStyle: {
+    color: '#ffffff',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  commonStyle: {},
+  // Works only for ios
+  boxShadow: {
+    shadowColor: '#333333',
+    shadowOffset: {
+      height: 10,
+      width: 10,
+    },
+    shadowOpacity: 0.6,
+    shadowRadius: 4,
   },
-  highlight: {
-    fontWeight: '700',
-  },
+  // for android
+  androidShadow:{
+    elevation: 10
+  }
 });
 
 export default App;
